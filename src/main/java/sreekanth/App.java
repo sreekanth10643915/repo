5d73d25569565b6c54aa8eb16d68b323a4ec3a11
sreekanth10643915 <sreekanth.10643915@gmail.com>
2018-03-20 13:08:13 +0530

    my project

---
+++ src/main/java/sreekanth/App.java
@@ -0,0 +1,13 @@
+package sreekanth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
